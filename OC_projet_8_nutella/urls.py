52dc37d0ca8bf3d6bb93350b15130667d0384337
jerem33620 <jeremyguy@orange.fr>
2019-11-27 15:46:55 +0100

    Actualisation du fichier OC_projet_8_nutella/urls.py

--- OC_projet_8_nutella/urls.py
+++ OC_projet_8_nutella/urls.py
@@ -22,8 +22,4 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
-    path('research/', views.research, name='research'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
 ]
