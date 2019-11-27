90106134240f49602a84cf07e02ae8912a3593ea
jerem33620 <jeremyguy@orange.fr>
2019-11-27 15:38:29 +0100

    Actualisation du fichier products/views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def research(request):
+    form = SearchForm(request.GET or None)
+
+    if form.is_valid():
+        product = form.cleaned_data['product']
+    return render(request, 'research.html')
