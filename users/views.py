010fee89daf2cb72ab1a306116371eea72b0354e
jerem33620 <jeremyguy@orange.fr>
2019-11-27 15:37:59 +0100

    Actualisation du fichier users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.urls import reverse
 
-# Create your views here.
+from home.forms import ConnexionForm, SearchForm, SingupForm
+from .models import User
+
+def login(request):
+    """Pour pouvoir se connecter"""
+    error = False
+
+    if request.method == "POST":
+        form = ConnexionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = auth.authenticate(username=username, password=password)
+            # Nous vérifions si les données sont correctes
+            if user:  # Si l'objet renvoyé n'est pas None
+                auth.login(request, user)
+                context = {
+                    "user": request.user,
+                }
+                return redirect("home")
+            else: # sinon une erreur sera affichée
+                error = True
+    else:
+        form = ConnexionForm()
+    context = {"form":form}
+
+    return render(request, 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect("home")
+
+def signup(request):
+    if request.method == 'POST':
+        form = SingupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SingupForm()
+    return render(request, 'signup.html', {'form': form})
