bcd1b9206d51d24cef7669269a7b3bdb4439be94
jerem33620 <jeremyguy@orange.fr>
2020-01-24 17:50:06 +0100

    Modification du fichier forms.py de l'application home

--- home/forms.py
+++ home/forms.py
@@ -1,24 +1,25 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from users.models import User
 
 
 class ConnexionForm(forms.Form):
     """ Cette class sert à pouvoir utiliser l'username et le password pour se connecter """
+
     username = forms.CharField(label="Nom d'utilisateur", max_length=20)
     password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput, min_length=6, max_length=16)
 
 class SearchForm(forms.Form):
     """ Cette class sert à montré le champd de recherche sur le site pour trouvé le produit demandé """
+    
     product = forms.CharField(
         max_length=100, 
         widget=forms.TextInput(attrs={'placeholder': "Champs de recherche"})
 )
 
-class SingupForm(UserCreationForm):
+class SignupForm(UserCreationForm):
     """ Cette class sert à enregistrer username, ces 2 password et l'email """
-    email = forms.CharField(label="email")
 
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "password1", "password2")
+        fields = ("username", "email")
