c239ec482bb869c0ef1c77d2a54bf62f7e45a93b
jerem33620 <jeremyguy@orange.fr>
2020-03-13 17:37:19 +0100

    Modification des dossiers scss et templates

---
+++ home/tests/test_views.py
@@ -0,0 +1,7 @@
+from django.core.urlresolvers import reverse
+from home import views
+
+class TestIndexPageTestCase(TestCase):
+    def test_index_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
