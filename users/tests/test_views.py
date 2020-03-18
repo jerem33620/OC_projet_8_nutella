9130a38614c0e0d2e9ed2161d11337138158cd58
jerem33620 <jeremyguy@orange.fr>
2020-03-18 16:25:53 +0100

    Modification du fichier users/tests/test_views.py

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -5,27 +5,20 @@ from users.models import User
 from users.views import login, logout, signup, accountlog
 
 
-@classmethod
-def test_data(self):
-    self.user = User.objects.create_user(
-        username = "test",
-        email = "test@test.fr",
-        password = "tests1",
-    )
-    self.user_profile = User(user=self.user)
+class TestUsersPageTestCase(TestCase):
 
-def test_login(self):
-    response = self.client.get(reverse('login'))
-    self.assertEqual(response.status_code, 200)
+    def test_login(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
 
-def test_logout(self):
-    response = self.client.get(reverse('logout'))
-    self.assertEqual(response.status_code, 200)
+    def test_logout(self):
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
 
-def test_signup(self):
-    response = self.client.get(reverse('signup'))
-    self.assertEqual(response.status_code, 200)
+    def test_signup(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
 
-def test_account(self):
-    response = self.client.get(reverse('account'))
-    self.assertEqual(response.status_code, 200)
+    def test_accountlog(self):
+        response = self.client.get(reverse('accountlog'))
+        self.assertEqual(response.status_code, 200)
