c1476c84739009214708092936c1c7d3343f94e5
jerem33620 <jeremyguy@orange.fr>
2020-03-25 10:46:31 +0100

    Modification des fichiers test_models.py et test_views.py

--- favorites/tests/test_views.py
+++ favorites/tests/test_views.py
@@ -4,3 +4,11 @@ from django.urls import reverse
 from favorites.views import favorite_list, favorite_save
 
 
+class TestFavoritesPageTestCase(TestCase):
+    def test_favorite_list_page(self):
+        response = self.client.get(reverse('favorite_list'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_favorite_save_page(self):
+        response = self.client.get(reverse('favorite_save'))
+        self.assertEqual(response.status_code, 302)
