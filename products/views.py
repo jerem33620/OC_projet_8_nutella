965f41eeb0e31c6aff0601dddca60a7cf5900802
jerem33620 <jeremyguy@orange.fr>
2020-04-07 16:30:55 +0200

    Actualisation du fichier views.py

--- products/views.py
+++ products/views.py
@@ -12,7 +12,7 @@ def research(request):
     if request.method == "GET":
         research_form = SearchForm(request.GET or None)
         if research_form.is_valid():
-            search_name = research_form.clean_data["product"]
+            search_name = research_form.cleaned_data["product"]
             substitutes, product = Product.objects.find_products(search_name)
             product.image_url = product.image_url.replace("400", "full")
     return render(request, "research.html", {"substitutes": substitutes, "product": product, "form": form})
